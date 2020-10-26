a365af8957a4f374e754487842cba7974f9221ca
Hannah Catherine Trask <hctrask@gmail.com>
2020-10-26 08:34:52 -0600

    food routes all functioning, including seed

--- controllers/mood.js
+++ controllers/mood.js
@@ -1,7 +1,9 @@
 const Mood = require("../models/mood")
 const { Router } = require("express")
 const router = Router()
-const moodData = require('../db/seedMood.json')
+const moodData = require('../db/seedMood.json');
+const { db } = require("../models/mood");
+const Food = require("../models/food");
 
 //seed route -- **NOT SURE IF WE NEED THIS?
 router.get('/seed', async (req, res) => {
@@ -15,27 +17,41 @@ router.get('/seed', async (req, res) => {
 });
 
 //index route
-router.get("/moods", async (req, res) => {
-    res.json(await Mood.find({}))
+router.get("/", async (req, res) => {
+    const moods = await Mood.find({}).populate('foods')
+    res.json({ status: 200, data: moods })
+    db.close()
+})
+
+//get one mood by id
+router.get('/:id', async (req, res) => {
+    const mood = await Mood.findById(req.params.id)
+    res.json( {status: 200, data: mood })
 })
 
 //create route
-router.post("/moods", async (req, res) => {
-    res.json(await Mood.create(req.body))
+router.post("/", async (req, res) => {
+    const mood = await Mood.create(req.body)
+    res.json({ status: 200, data: mood })
 })
 
 //update route - add food to mood by id
 router.put("/:moodsId/addFoods/:foodsId", async (req, res) => {
-    res.json(await Mood.findByIdAndUpdate(req.params.id, req.food, { new: true }))
+    const food = await Food.findById(req.params.foodsId)
+    const mood = await Mood.findByIdAndUpdate(
+        req.params.moodsId,
+        { $push: {foods: food.id}, new: true }
+        )
+        res.json({ status: 200, data: mood })
 })
 
-//get by id route
-router.get("/moods/:id", async (req, res) => {
-    res.json(await Mood.findById(req.params.id))
-})
+// //get by id route
+// router.get("/moods/:id", async (req, res) => {
+//     res.json(await Mood.findById(req.params.id))
+// })
 
 //delete route
-router.delete("/moods/:id", async (req, res) => {
+router.delete("/:id", async (req, res) => {
     res.json(await Mood.findByIdAndRemove(req.params.id))
 })
 
