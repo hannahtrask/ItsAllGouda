bac089373b561365bdc9dc044e3885307e6c91b1
Chase Wood <chase@Chases-MacBook-Pro.local>
2020-10-27 09:26:22 -0500

    added new routes for the home and display components

--- controllers/food.js
+++ controllers/food.js
@@ -1,43 +1,45 @@
-const Food = require("../models/food")
-const { Router } = require("express")
-const router = Router()
-const foodData = require('../db/seedFood.json')
+const Food = require('../models/food');
+const { Router } = require('express');
+const router = Router();
+const foodData = require('../db/seedFood.json');
 
 //seed route -- this works.
-router.get('/seed', async (req, res) => {
-    try {
-        await Food.deleteMany({});
-        const food = await Food.insertMany(foodData);
-        res.json({ status: 200, data: food });
-    } catch (err) {
-        res.status(400).json({ err });
-    }
+router.get('/foods/seed', async (req, res) => {
+	try {
+		await Food.deleteMany({});
+		const food = await Food.insertMany(foodData);
+		res.json({ status: 200, data: food });
+	} catch (err) {
+		res.status(400).json({ err });
+	}
 });
 
 //index route
-router.get("/foods", async (req, res) => {
-    res.json(await Food.find({}))
-})
+router.get('/foods', async (req, res) => {
+	res.json(await Food.find({}));
+});
 
 //create route
-router.post("/foods", async (req, res) => {
-    res.json(await Food.create(req.body))
-})
+router.post('/foods', async (req, res) => {
+	res.json(await Food.create(req.body));
+});
 
 //get one by id route
-router.get("/foods/:id", async (req, res) => {
-    res.json(await Food.findById(req.params.id))
-})
+router.get('/foods/:id', async (req, res) => {
+	res.json(await Food.findById(req.params.id));
+});
 
 //update one by id route
-router.put("/foods/:id", async (req, res) => {
-    res.json(await Food.findByIdAndUpdate(req.params.id, req.body, { new: true }))
-})
+router.put('/foods/:id', async (req, res) => {
+	res.json(
+		await Food.findByIdAndUpdate(req.params.id, req.body, { new: true })
+	);
+});
 
 //delete one by id route
-router.delete("/foods/:id", async (req, res) => {
-    res.json(await Food.findByIdAndRemove(req.params.id))
-})
+router.delete('/foods/:id', async (req, res) => {
+	res.json(await Food.findByIdAndRemove(req.params.id));
+});
 
 //export router
-module.exports = router
+module.exports = router;
--- controllers/mood.js
+++ controllers/mood.js
@@ -1,43 +1,60 @@
-const Mood = require("../models/mood")
-const { Router } = require("express")
-const router = Router()
-const moodData = require('../db/seedMood.json')
+const Mood = require('../models/mood');
+const Food = require('../models/food');
+const { Router } = require('express');
+const router = Router();
+const moodData = require('../db/seedMood.json');
+const { STATES } = require('mongoose');
 
 //seed route -- **NOT SURE IF WE NEED THIS?
 router.get('/moods/seed', async (req, res) => {
-    try {
-        await Mood.deleteMany({});
-        const mood = await Mood.insertMany(moodData);
-        res.json({ status: 200, data: mood });
-    } catch (err) {
-        res.status(400).json({ err });
-    }
+	try {
+		await Mood.deleteMany({});
+		const mood = await Mood.insertMany(moodData);
+		res.json({ status: 200, data: mood });
+	} catch (err) {
+		res.status(400).json({ err });
+	}
 });
 
 //index route
-router.get("/moods", async (req, res) => {
-    res.json(await Mood.find({}))
-})
+router.get('/moods', async (req, res) => {
+	res.json(await Mood.find({}).populate('foods'));
+});
+
+//get route by name:happy
+router.get('/moods/happy', async (req, res) => {
+	res.json(await Mood.find({ name: 'Happy' }).populate('foods'));
+});
+
+//get route by name:sad
+router.get('/moods/sad', async (req, res) => {
+	res.json(await Mood.find({ name: 'Sad' }).populate('foods'));
+});
 
 //create route
-router.post("/moods", async (req, res) => {
-    res.json(await Mood.create(req.body))
-})
+router.post('/moods', async (req, res) => {
+	res.json(await Mood.create(req.body));
+});
 
-//update route - add food to mood by id
-router.put("/:moodsId/addFoods/:foodsId", async (req, res) => {
-    res.json(await Mood.findByIdAndUpdate(req.params.id, req.food, { new: true }))
-})
+//update route - add food to mood by id - could not get this to work before so i copied the one from fruits/owner to get this
+router.put('/:moodsId/addFoods/:foodsId', async (req, res) => {
+	const food = await Food.findById(req.params.foodsId);
+	const mood = await Mood.findByIdAndUpdate(req.params.moodsId, {
+		$push: { foods: food.id },
+		new: true,
+	});
+	res.json({ status: 200, data: mood });
+});
 
 //get by id route
-router.get("/moods/:id", async (req, res) => {
-    res.json(await Mood.findById(req.params.id))
-})
+router.get('/moods/:id', async (req, res) => {
+	res.json(await Mood.findById(req.params.id));
+});
 
 //delete route
-router.delete("/moods/:id", async (req, res) => {
-    res.json(await Mood.findByIdAndRemove(req.params.id))
-})
+router.delete('/moods/:id', async (req, res) => {
+	res.json(await Mood.findByIdAndRemove(req.params.id));
+});
 
 //export router
-module.exports = router
+module.exports = router;
